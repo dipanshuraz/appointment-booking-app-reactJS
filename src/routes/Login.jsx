1740bc4b67faac6bf16a2dd163cac7867f55571b
Deepanshu Prajapati <dipanshuraz2@gmail.com>
2020-02-27 20:33:53 +0530

    Login Page

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -4,8 +4,65 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <>
-      <div>LOGIN PAGE</div>
-      <Link to="/dash">GOTO DASHBOARD</Link>
+      <div className="container mt-5">
+        <button type="button" className="btn btn-light border btn-sm">
+          <Link to="/dash">
+            <i className="fas fa-arrow-left border rounded-circle" /> GOTO
+            DASHBOARD
+          </Link>
+        </button>
+        <div className="row">
+          <div className="col-md-6 ">
+            <div className="row d-flex align-items-center h-100">
+              <div className="col-md-2 " />
+              <div className="col-md-8">
+                <button
+                  type="button"
+                  className="btn btn-lg btn-primary border btn-block text-left"
+                >
+                  <i className="fab fa-google" /> Gmail
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-lg btn-secondary border btn-block text-left"
+                >
+                  <i className="fab fa-facebook-f" /> Facebook
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-lg btn-light border btn-block text-left"
+                >
+                  <i className="far fa-envelope" /> Email
+                </button>
+                <form action="">
+                  <div className="form-group">
+                    <input
+                      className="form-control form-control-lg my-2"
+                      type="text"
+                      placeholder="Email"
+                    />
+                    <input
+                      className="form-control form-control-lg my-2"
+                      type="text"
+                      placeholder="Password"
+                    />
+                    <button
+                      type="button"
+                      className="btn btn-lg btn-light border btn-block text-left"
+                    >
+                      <i className="far fa-envelope" /> Login
+                    </button>
+                  </div>
+                </form>
+              </div>
+              <div className="col-md-2 " />
+            </div>
+          </div>
+          <div className="col-md-6">
+            <img src="./images/ofc.png" className="img-fluid" alt="" />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
