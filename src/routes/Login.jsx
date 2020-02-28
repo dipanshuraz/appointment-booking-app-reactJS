8b609e08b3ca125b8385b3481ebe89d552e4b4ba
Deepanshu Prajapati <dipanshuraz2@gmail.com>
2020-02-28 18:37:45 +0530

    404 Fixed

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,80 +1,91 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-const Login = () => {
-  let bool = false;
-  return (
-    <>
-      <div className="container mt-5">
-        <button type="button" className="btn btn-light border btn-sm">
-          <Link to="/dash">
-            <i className="fas fa-arrow-left border rounded-circle" /> GOTO
-            DASHBOARD
-          </Link>
-        </button>
-        <div className="row">
-          <div className="col-md-6 ">
-            <div className="row d-flex align-items-center h-100">
-              <div className="col-md-2 " />
-              <div className="col-md-8">
-                <button
-                  type="button"
-                  className="btn btn-lg btn-light border btn-block text-left"
-                  onClick={() => {
-                    bool = true;
-                  }}
-                >
-                  <i className="far fa-envelope" /> Email
-                </button>
+export class Login extends Component {
+  constructor(props) {
+    super(props);
 
-                {bool ? (
-                  <form action="">
-                    <div className="form-group">
-                      <input
-                        className="form-control form-control-lg my-2"
-                        type="text"
-                        placeholder="Email"
-                      />
-                      <input
-                        className="form-control form-control-lg my-2"
-                        type="text"
-                        placeholder="Password"
-                      />
-                      <button
-                        type="button"
-                        className="btn btn-lg btn-primary border btn-block text-center"
-                      >
-                        Submit
+    this.state = {
+      bool: false
+    };
+  }
+
+  onClickEmail = () => {
+    this.setState(prev => ({ bool: !prev.bool }));
+  };
+
+  render() {
+    return (
+      <>
+        <div className="container mt-5">
+          <button type="button" className="btn btn-light border btn-sm my-3">
+            <Link to="/dash">
+              <i className="fas fa-arrow-left border rounded-circle" /> GOTO
+              DASHBOARD
+            </Link>
+          </button>
+          <div className="row">
+            <div className="col-md-6 ">
+              <div className="row d-flex align-items-center h-100">
+                <div className="col-md-2 " />
+                <div className="col-md-8">
+                  <button
+                    type="button"
+                    className="btn btn-lg btn-light border btn-block text-left"
+                    onClick={this.onClickEmail}
+                  >
+                    <i className="far fa-envelope" /> Email
+                  </button>
+
+                  {this.state.bool ? (
+                    <form action="">
+                      <div className="form-group">
+                        <input
+                          className="form-control form-control-lg my-2"
+                          type="text"
+                          placeholder="Email"
+                        />
+                        <input
+                          className="form-control form-control-lg my-2"
+                          type="text"
+                          placeholder="Password"
+                        />
+                        <button
+                          type="button"
+                          className="btn btn-lg btn-primary border btn-block text-center"
+                        >
+                          Submit
+                        </button>
+                      </div>
+                    </form>
+                  ) : (
+                      <>
+                        <button
+                          type="button"
+                          className="btn btn-lg btn-primary border btn-block text-left"
+                        >
+                          <i className="fab fa-google" /> Gmail
                       </button>
-                    </div>
-                  </form>
-                ) : (
-                  <>
-                    <button
-                      type="button"
-                      className="btn btn-lg btn-primary border btn-block text-left"
-                    >
-                      <i className="fab fa-google" /> Gmail
-                    </button>
-                    <button
-                      type="button"
-                      className="btn btn-lg btn-secondary border btn-block text-left"
-                    >
-                      <i className="fab fa-facebook-f" /> Facebook
-                    </button>
-                  </>
-                )}
+                        <button
+                          type="button"
+                          className="btn btn-lg btn-secondary border btn-block text-left"
+                        >
+                          <i className="fab fa-facebook-f" /> Facebook
+                      </button>
+                      </>
+                    )}
+                </div>
+                <div className="col-md-2 " />
               </div>
-              <div className="col-md-2 " />
+            </div>
+            <div className="col-md-6">
+              <img src="./images/ofc.png" className="img-fluid" alt="" />
             </div>
           </div>
-          <div className="col-md-6">
-            <img src="./images/ofc.png" className="img-fluid" alt="" />
-          </div>
         </div>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  }
+}
 
 export default Login;
--- src/routes/NoMatch.jsx
+++ src/routes/NoMatch.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../index.css";
 
 const NoMatch = () => {
-  return <div>ERROR: PAGE NOT FOUND</div>;
+  return (
+    <div className="container h-70">
+      <div className="row justify-content-center ">
+        <img src="./images/404.jpg" className="img-fluid" alt="" />
+        <div className="col-md-4 col-sm">
+          <hr />
+          <p className="lead text-center">Go To Home</p>
+          <hr />
+          <center>
+            <Link to="/">
+              <i className="fa-3x fas fa-home" />
+            </Link>
+          </center>
+          <hr />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NoMatch;
