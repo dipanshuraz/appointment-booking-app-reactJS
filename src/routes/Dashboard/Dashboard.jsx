7c0f8bad61607ebffa21b107dd7ff1c6a8f6862f
shiv-ansh <shivansh.121996@gmail.com>
2020-02-29 10:32:18 +0530

    User DashBoard Updated

--- src/routes/Dashboard/Dashboard.jsx
+++ src/routes/Dashboard/Dashboard.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import AdminDashboard from "./AdminDashboard";
+import UserDashboard from "./UserDashboard";
 
 const Dashboard = () => {
-  return <div>DASHBOARD HOME</div>;
+  return (
+    <>
+      <div>DASHBOARD HOME</div>
+      <UserDashboard />
+      <AdminDashboard />
+    </>
+  );
 };
 
 export default Dashboard;
