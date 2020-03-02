3765a6c4763fb433206cd346105e36c507c22854
Deepanshu Prajapati <dipanshuraz2@gmail.com>
2020-03-02 16:14:37 +0530

    admin-dashboard

--- src/components/others/BookingCard.jsx
+++ src/components/others/BookingCard.jsx
@@ -1,43 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class BookingCard extends Component {
   render() {
     return (
       <div className="container border w-100 my-3 p-3">
         <div className="row text-center">
-          <div className="col-md-4 ">
-            8 am - 10 pm
-          </div>
-          <div className="col-md-2">
-            100 ₹
-          </div>
-          <div className="col-md-6">
-            Barber
-          </div>
+          <div className="col-md-4 ">8 am - 10 pm</div>
+          <div className="col-md-2">100 ₹</div>
+          <div className="col-md-6">Barber</div>
         </div>
         <div className="row">
           <div className="col-md-4">
-            <i class="fas fa-rupee-sign mx-2"></i>
+            <i className="fas fa-rupee-sign mx-2" />
             Online
           </div>
-          <div className="col-md-4">
-            One to One
-          </div>
+          <div className="col-md-4">One to One</div>
           <div className="col-md-2">
-            <button className='btn btn-block btn-outline-danger btn-block btn-sm '>Cancel</button>
+            <button className="btn btn-block btn-outline-danger btn-block btn-sm ">
+              Cancel
+            </button>
           </div>
         </div>
-      </div >
-    )
+      </div>
+    );
   }
 }
 
-export default BookingCard
+export default BookingCard;
 
-
-{/* < div className = "col-md-2 bg-light" >
+{
+  /* < div className = "col-md-2 bg-light" >
     <button className='btn btn-block btn-outline-success btn-block btn-sm'>Update</button>
           </div >
   <div className="col-md-2">
    
-  </div> */}
+  </div> */
+}
--- src/routes/Dashboard/AdminDashboard.jsx
+++ src/routes/Dashboard/AdminDashboard.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import BookingCard from "../../components/others/BookingCard";
 
 const AdminDashboard = () => {
-
   return (
     <div className="container">
-
-      <p className='text-center py-3 lead'>Admin DashBord</p>
+      <p className="text-center py-3 lead">Admin DashBord</p>
       <div className="row">
         <div className="col-md-6">
           <p className="lead display-4 text-center my-3">Booked</p>
@@ -14,11 +12,12 @@ const AdminDashboard = () => {
           <BookingCard />
         </div>
         <div className="col-md-6">
-
           <p className="display-4 text-center my-4">Open Slots</p>
 
           <div className="row card m-3 border p-2 align-items-center text-dark">
-            <h4 class="card-title"><i class="fa-3x fas fa-users"></i></h4>
+            <h4 className="card-title">
+              <i className="fa-3x fas fa-users" />
+            </h4>
             <div className="col-md-8 text-center">
               <h1>Group Meet</h1>
             </div>
@@ -27,7 +26,9 @@ const AdminDashboard = () => {
             </button>
           </div>
           <div className="row card m-3 border p-2 align-items-center text-dark">
-            <h4 class="card-title"><i class="fa-3x fas fa-user-friends"></i></h4>
+            <h4 className="card-title">
+              <i className="fa-3x fas fa-user-friends" />
+            </h4>
             <div className="col-md-8 text-center">
               <h1>One on One Meet</h1>
             </div>
@@ -37,11 +38,8 @@ const AdminDashboard = () => {
           </div>
         </div>
       </div>
-    </div >
-
+    </div>
   );
 };
 
 export default AdminDashboard;
-
-
